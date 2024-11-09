ce8440883246d7f5b0efd03f740dba4ae59bd8cf
naga prasad <naga prasad@LAPTOP-6KJACAI6>
2024-11-09 13:45:36 +0530

    additional code

---
+++ project1/src/demo1/Sum.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Sum {
+  public int add(int a, int b)
+  {
+	  int c=a+b;
+	  return c;
+  }
+  
+
+}
---
+++ project1/src/demo1/SumMain.java
@@ -0,0 +1,10 @@
+package demo1;
+
+public class SumMain {
+	public static void main(String[] args) {
+		Sum s=new Sum();
+		int d=s.add(20, 10);
+		System.out.println(d);
+	}
+
+}
